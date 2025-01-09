a3b59bd31be7a9dfe3281a9b6fbd6848a8aabcde
Hyoju <89563205+oohyj@users.noreply.github.com>
2025-01-09 15:20:20 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int alpha_centauri(long long x, long long y) {
+    if (x == y) return 0;
+
+    long long max = 0, move = 0;
+
+    while (max * max <= y - x) {
+        max++;
+    }
+    max--; // 루프를 초과한 상태에서 종료했으므로 1 감소
+
+    move = 2 * max - 1;
+
+    long long rem = y - x - (max * max);
+    rem = (rem + max - 1) / max;
+
+    move += rem;
+    return move;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long x, y;
+        cin >> x >> y;
+        cout << alpha_centauri(x, y) << '\n';
+    }
+    return 0;
+}
