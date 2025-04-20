87f3f2fc7e9b250e635655c1e2500e3fcc986663
Hyoju <89563205+oohyj@users.noreply.github.com>
2025-04-20 23:28:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    int n = commands.size();
+    for (int i = 0; i < n; ++i) {
+        vector<int> v;
+        for (int j = commands[i][0] - 1; j < commands[i][1]; ++j) {
+            v.push_back(array[j]);
+        }
+        sort(v.begin(), v.end());
+        answer.push_back(v[commands[i][2] - 1]);
+    }
+    return answer;
+}
