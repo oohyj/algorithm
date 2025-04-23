e4994e6c702464a5e11d3403275d184310373c51
Hyoju <89563205+oohyj@users.noreply.github.com>
2025-04-24 01:41:42 +0900

    [Gold IV] Title: 리모컨, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int N, M;
+vector<bool> isBroken(10, false);
+
+bool isValid(int num) {
+    if (num == 0) return !isBroken[0];
+    while (num > 0) {
+        if (isBroken[num % 10]) return false;
+        num /= 10;
+    }
+    return true;
+}
+
+int getLength(int num) {
+    if (num == 0) return 1;
+    int len = 0;
+    while (num > 0) {
+        len++;
+        num /= 10;
+    }
+    return len;
+}
+
+int main() {
+    cin >> N >> M;
+    while (M--) {
+        int btn;
+        cin >> btn;
+        isBroken[btn] = true;
+    }
+
+    int minPress = abs(N - 100);
+
+    for (int i = 0; i <= 999999; ++i) {
+        if (isValid(i)) {
+            int press = getLength(i) + abs(N - i);
+            minPress = min(minPress, press);
+        }
+    }
+
+    cout << minPress << '\n';
+    return 0;
+}
