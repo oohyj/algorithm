2eaae6ad31b773f744ef7cc64663d536db0296bd
Hyoju <89563205+oohyj@users.noreply.github.com>
2025-05-09 15:23:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 6.63 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -1,43 +1,41 @@
-
+#include <string>
 #include <vector>
 #include <queue>
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
-    queue<int> q1, q2;
-    long long sum1 = 0, sum2 = 0;
-
-    for (int n : queue1) {
-        q1.push(n);
-        sum1 += n;
+    queue<int> q1 , q2;
+    long long sum1 = 0 , sum2 = 0;
+    for(int i =0; i<queue1.size(); ++i){
+        sum1 += queue1[i];
+        q1.push(queue1[i]);
+        sum2 += queue2[i];
+        q2.push(queue2[i]);
     }
-
-    for (int n : queue2) {
-        q2.push(n);
-        sum2 += n;
-    }
-
-    int maxCount = queue1.size() * 3;
+    
+    int maxCount = queue1.size()*3;
     int count = 0;
-
-    while (count <= maxCount) {
-        if (sum1 == sum2) return count;
-
-        if (sum1 > sum2) {
-            if (q1.empty()) return -1;
-            int n = q1.front(); q1.pop();
+    while(count <= maxCount){
+        if(sum1 == sum2){
+           return count;
+        }
+        if(sum1 > sum2){
+            if(q1.empty()) return -1;
+            int n = q1.front();
+            q1.pop();
             q2.push(n);
             sum1 -= n;
             sum2 += n;
-        } else {
-            if (q2.empty()) return -1;
-            int n = q2.front(); q2.pop();
-            q1.push(n);
-            sum2 -= n;
-            sum1 += n;
         }
-        count++;
+        else{
+            if(q2.empty()) return -1;
+            int n = q2.front();
+            q2.pop();
+              q1.push(n);
+            sum1 +=n;
+            sum2 -=n;
+        }
+        ++count;
     }
-
     return -1;
 }
