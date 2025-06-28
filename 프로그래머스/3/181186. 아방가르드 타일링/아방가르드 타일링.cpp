9a7d41dd82ceb949e34d99b08cd733fec7608efd
Hyoju <89563205+oohyj@users.noreply.github.com>
2025-06-28 19:55:45 +0900

    [level 3] Title: 아방가르드 타일링, Time: 1.66 ms, Memory: 4.41 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cpp
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.cpp
@@ -1,27 +1,30 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+#define DIV 1000000007
+
+vector<unsigned long long> dp(100001, 0);
+vector<unsigned long long> sum(100001, 0);
+
+
 int solution(int n) {
-    //n = 4;
-    int ans[4] = {0,1,3,10};
-    if(n <= 3) return ans[n];
+    sum[1] = dp[1] = 1;
+    sum[2] = dp[2] = 3;
+    sum[3] = dp[3] = 10;
 
-    vector<long long> dp(n+1,0);
-    vector<long long> dpA(n+1,0);
-    vector<long long> dpB(n+1,0);
-    dp[0] = 1; dpA[0] = 1; dpB[0] = 1;
-    dp[1] =1; dp[2] = 3; dp[3] = 10;
-    dpA[1] = 0; dpA[2] = 0; dpA[3] = 1;
-    dpB[1] = 0; dpB[2] = 0; dpB[3] = 1;
-
-    for(int i=4; i<=n;i++){
-        dpA[i] = (dp[i-3] + dpB[i-2]) % 1000000007 ;
-        dpB[i] = (dp[i-3] + dpA[i-1]) % 1000000007 ;
-        dp[i] = (dp[i-3] + dp[i-2]*2 + dp[i-1] + dpA[i]*2 + dpB[i]*2) % 1000000007 ;
+    for(int i = 4; i <= n; i++){
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2 + dp[i - 3] * 5) % DIV;
+        if(i > 4) dp[i] = (dp[i] + sum[i - 4] * 2) % DIV;
+        if(i > 5) dp[i] = (dp[i] + sum[i - 5] * 2) % DIV;
+        if(i > 6) dp[i] = (dp[i] + sum[i - 6] * 4) % DIV;
+        if(i % 3 == 1) dp[i] = (dp[i] + 2) % DIV;
+        else if(i % 3 == 2) dp[i] = (dp[i] + 2) % DIV;
+        else if(i % 3 == 0) dp[i] = (dp[i] + 4) % DIV;
+        sum[i] = (sum[i - 3] + dp[i]) % DIV;
     }
+    return dp[n] % DIV;
 
-    long long answer = dp[n];
-    return answer;
 }
